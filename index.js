a39552fbd3029588e2cd344081284042238105b0
BukerBes <bukerbes1@gmail.com>
2024-07-20 21:32:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+/* eslint-env es6 */
+/* eslint-disable no-console */
+const coinBtn = document.querySelector('.coin');
+const handleClick = (event) => {
+    console.log('Click');
+    const clickValString = document.querySelector('.income-val-tap');
+    const coinCount = document.querySelector('.count-name');
+    const clickVal = clickValString.textContent.substring(1);
+    
+    let newVal = Number(coinCount.textContent) + Number(clickVal);
+    
+    coinCount.textContent = newVal;
+}
+
+coinBtn.addEventListener('click', handleClick);
